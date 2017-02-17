61dd066be4029f59bb9faa442f48895b7ea1027a
Mike Aldred <mike.aldred@luminousmonkey.org>
2017-02-17 10:35:55 +0800

    Moved UI resources, updated dependencies versions.

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject clojure-miniprofiler "0.5.0"
   :description "a simple but effective profiler for clojure web applications"
-  :resource-paths ["resources"]
+  :resource-paths ["resources/miniprofiler-ui"]
   :url "http://yellerapp.com/opensource/clojure-miniprofiler.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.3.1"]
-                 [ring/ring-core "1.3.2"]
-                 [fipp "0.5.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.7.0"]
+                 [ring/ring-core "1.5.1"]
+                 [fipp "0.6.9"]]
   :profiles {:dev {:dependencies [[compojure "1.1.8"]]
                    :plugins [[lein-ring "0.8.11"]]
                    :ring {:handler clojure-miniprofiler.example/app}
                    :source-paths ["dev"]}
-             :bench {:dependencies [[criterium "0.4.1"]
-                                    [ring "1.3.2"]
-                                    [compojure "1.1.8"]]
+             :bench {:dependencies [[criterium "0.4.4"]
+                                    [ring "1.5.1"]
+                                    [compojure "1.5.2"]]
                      :main clojure-miniprofiler.bench
                      :source-paths ["src" "bench" "dev"]
                      :jvm-opts ^:replace
